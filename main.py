1c8058e268d0ac3a6c95b5f07875ac82e684019d
Nella1a <m.allen@gmx.at>
2025-08-06 10:33:04 +0200

    add value for total inventory price and create new file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ for product_row in range(2, product_list.max_row + 1):
     inventory = product_list.cell(product_row, 2).value
     price = product_list.cell(product_row, 3).value
     product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
 
     # calculation number of products / supplier
     if supplier_name in products_per_supplier:
@@ -30,7 +31,13 @@ for product_row in range(2, product_list.max_row + 1):
     if inventory < 10:
         products_under_10_inv[int(product_num)] = int(inventory)
 
+    # add value for total inventory price
+    inventory_price.value = inventory * price
+
 
 print(products_per_supplier)
 print(total_value_per_supplier)
 print(products_under_10_inv)
+
+inv_file.save("inventory_with_total_value.xlsx")
+
